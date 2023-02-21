5ffeedec7645ba5425b5499d3ee8769a98a5e966
OksanaN <nurova.oksana2020@gmail.com>
2023-02-21 11:51:29 +0500

    stading immutability

--- src/10 mmutability/10.test.tsx
+++ src/10 mmutability/10.test.tsx
@@ -1,7 +1,15 @@
-import {upgradeUserLaptop, UserType} from "./10";
+import {
+    removeUserBooks,
+    replaceUserBooks,
+    updateUserCompanie,
+    upgradeUserBooks,
+    upgradeUserLaptop,
+    UserType,
+    WithCompaniesType
+} from "./10";
 
 test('upgrate user`s laptop', () => {
-    const user: UserType  = {
+    const user: UserType = {
         name: 'Ingvarr',
         address: {
             city: 'Dushanbe',
@@ -9,7 +17,8 @@ test('upgrate user`s laptop', () => {
         },
         laptop: {
             title: 'Macbook pro 2018'
-        }
+        },
+        books: ['html & css', 'js', 'react']
     }
 
     const upgradeUser = upgradeUserLaptop(user, 'Macbook pro 2023')
@@ -19,4 +28,94 @@ test('upgrate user`s laptop', () => {
     expect(user.laptop).not.toBe(upgradeUser.laptop)
     expect(upgradeUser.laptop.title).toBe('Macbook pro 2023')
     expect(user.laptop.title).toBe('Macbook pro 2018')
+})
+
+test('upgrate user`s books', () => {
+    const user: UserType = {
+        name: 'Ingvarr',
+        address: {
+            city: 'Dushanbe',
+            house: 23
+        },
+        laptop: {
+            title: 'Macbook pro 2018'
+        },
+        books: ['html & css', 'js', 'react']
+    }
+
+    const upgradeUser = upgradeUserBooks(user, ['ts', 'redux'])
+
+    expect(user).not.toBe(upgradeUser)
+    expect(user.address).toBe(upgradeUser.address)
+    expect(user.laptop).toBe(upgradeUser.laptop)
+    expect(upgradeUser.books).not.toBe(user.books)
+    expect(upgradeUser.books[3]).toBe('ts')
+    expect(upgradeUser.books[4]).toBe('redux')
+})
+
+test('replace user`s books', () => {
+    const user: UserType = {
+        name: 'Ingvarr',
+        address: {
+            city: 'Dushanbe',
+            house: 23
+        },
+        laptop: {
+            title: 'Macbook pro 2018'
+        },
+        books: ['html & css', 'js', 'react']
+    }
+
+    const upgradeUser = replaceUserBooks(user, 'js', 'ts')
+
+    expect(user).not.toBe(upgradeUser)
+    expect(user.address).toBe(upgradeUser.address)
+    expect(user.laptop).toBe(upgradeUser.laptop)
+    expect(upgradeUser.books).not.toBe(user.books)
+    expect(upgradeUser.books[1]).toBe('ts')
+})
+
+test('remove user`s books', () => {
+    const user: UserType = {
+        name: 'Ingvarr',
+        address: {
+            city: 'Dushanbe',
+            house: 23
+        },
+        laptop: {
+            title: 'Macbook pro 2018'
+        },
+        books: ['html & css', 'js', 'react']
+    }
+
+    const upgradeUser = removeUserBooks(user, 'js')
+
+    expect(user).not.toBe(upgradeUser)
+    expect(user.address).toBe(upgradeUser.address)
+    expect(user.laptop).toBe(upgradeUser.laptop)
+    expect(upgradeUser.books).not.toBe(user.books)
+    expect(upgradeUser.books[1]).toBe('react')
+})
+
+test('update user`s company', () => {
+    const user: UserType & WithCompaniesType = {
+        name: 'Ingvarr',
+        address: {
+            city: 'Dushanbe',
+            house: 23
+        },
+        laptop: {
+            title: 'Macbook pro 2018'
+        },
+        books: ['html & css', 'js', 'react'],
+        companies: [{id: 1, title: 'justice-it'}, {id: 2, title: 'google'}]
+    }
+
+    const upgradeUser = updateUserCompanie(user, 1, 'fortech') as UserType & WithCompaniesType
+
+    expect(user).not.toBe(upgradeUser)
+    expect(user.address).toBe(upgradeUser.address)
+    expect(user.laptop).toBe(upgradeUser.laptop)
+    expect(upgradeUser.books).toBe(user.books)
+    expect(upgradeUser.companies[0].title).toBe('fortech')
 })
--- src/10 mmutability/10.tsx
+++ src/10 mmutability/10.tsx
@@ -11,10 +11,19 @@ export type UserType = {
     name: string
     address: AdressType
     laptop: LaptopType
+    books: Array<string>
 
 }
 
-export function upgradeUserLaptop(user: UserType, laptop: string) {
+export type WithCompaniesType = {
+    companies: Array<{
+        id: number
+        title: string
+    }>
+}
+
+
+export function upgradeUserLaptop(user: UserType, laptop: string): UserType {
     return {
         ...user,
         laptop: {
@@ -23,3 +32,32 @@ export function upgradeUserLaptop(user: UserType, laptop: string) {
         }
     }
 }
+
+export function upgradeUserBooks(user: UserType, book: Array<string>): UserType {
+    return {
+        ...user,
+        books: [...user.books, ...book]
+    }
+}
+
+export function replaceUserBooks(user: UserType, oldBook: string, newBook: string): UserType {
+    return {
+        ...user,
+        books: user.books.map(el => el === oldBook ? oldBook = newBook : el)
+    }
+}
+
+export function removeUserBooks(user: UserType, bookForRemove: string): UserType {
+    return {
+        ...user,
+        books: user.books.filter(el => el !== bookForRemove)
+    }
+}
+
+export function updateUserCompanie(user: WithCompaniesType, companyId: number, newCompany: string) {
+    return {
+        ...user,
+        companies: user.companies.map(el => companyId === el.id ? {...el, title: newCompany} : el)
+    }
+}
+
